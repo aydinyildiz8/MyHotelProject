7497d4766da0ebf8df3528bdb941c3a9237c8b72
Aydın Yıldız <113350420+aydinyildiz8@users.noreply.github.com>
2023-05-20 01:10:55 +0300

    Guest Trigger işlemi

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Guest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class Guest
+    {
+        public int GuestID { get; set; }
+        public int GuestTcNumber { get; set; }
+        public string GuestName { get; set; }
+        public string GuestSurName { get; set; }
+        public string GuestGender { get; set; }
+        public string GuestCity { get; set; }
+        public string GuestPhone { get; set; }
+        
+    }
+}
